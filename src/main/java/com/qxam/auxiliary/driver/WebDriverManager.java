5ea36c2a80520c80c980bf3ecab0b099fbf3bf63
Piotr Gliszczynski <gliszczynskip5@gmail.com>
2023-12-09 13:33:39 +0100

    feat: add creation of webdriver

---
+++ src/main/java/com/qxam/auxiliary/driver/WebDriverManager.java
@@ -0,0 +1,58 @@
+package com.qxam.auxiliary.driver;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.WebDriverRunner;
+import com.qxam.auxiliary.driver.factory.DriverFactory;
+import com.qxam.config.DataProvider;
+import com.qxam.config.TestDataProperties;
+import lombok.extern.slf4j.Slf4j;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+import java.util.Objects;
+
+@Slf4j
+public class WebDriverManager implements DriverManager {
+
+    private final ThreadLocal<WebDriver> threadWebDriver = new ThreadLocal<>();
+
+    @Override
+    public void instantiateDriver() {
+        log.info("About to init the new web getDriver instance");
+        WebDriverType driverType = WebDriverType.getDriverTypeFromSettings();
+
+        WebDriver driver = DriverFactory.getDriver(driverType);
+        driver.manage().window().maximize();
+
+        TestDataProperties properties = DataProvider.get();
+
+        Configuration.browser = driverType.name().toLowerCase();
+        Configuration.timeout = properties.waitTimeout();
+        Configuration.pageLoadTimeout = properties.pageLoadTimeout();
+
+        WebDriverRunner.setWebDriver(driver);
+        threadWebDriver.set(driver);
+    }
+
+    @Override
+    public void quit() {
+        if (Objects.nonNull(threadWebDriver.get())) {
+            log.info("Shutting down the getDriver");
+        }
+
+    }
+
+    @Override
+    public WebDriver get() {
+        log.info("Retrieving the getDriver");
+        return threadWebDriver.get();
+    }
+
+    @Override
+    public byte[] takeScreenshot() {
+        log.info("Taking screenshot");
+        return ((TakesScreenshot) threadWebDriver.get())
+                .getScreenshotAs(OutputType.BYTES);
+    }
+}
--- src/main/java/com/qxam/auxiliary/driver/factory/DriverFactory.java
+++ src/main/java/com/qxam/auxiliary/driver/factory/DriverFactory.java
@@ -5,8 +5,8 @@ import org.openqa.selenium.WebDriver;
 
 public abstract class DriverFactory {
 
-    public static WebDriver getDriver(String driver) {
-        return switch (WebDriverType.valueOf(driver)) {
+    public static WebDriver getDriver(WebDriverType driver) {
+        return switch (driver) {
             case CHROME -> new ChromeDriverFactory().createDriver();
             case FIREFOX -> new FirefoxDriverFactory().createDriver();
             case EDGE -> new EdgeDriverFactory().createDriver();
