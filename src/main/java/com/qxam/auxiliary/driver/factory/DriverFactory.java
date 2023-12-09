9fbda470c32ed029cdc402f14cef08d311c87269
Piotr Gliszczynski <gliszczynskip5@gmail.com>
2023-12-09 13:14:22 +0100

    fix: add factory method to DriverFactory

--- src/main/java/com/qxam/auxiliary/driver/factory/DriverFactory.java
+++ src/main/java/com/qxam/auxiliary/driver/factory/DriverFactory.java
@@ -1,11 +1,17 @@
 package com.qxam.auxiliary.driver.factory;
 
+import com.qxam.auxiliary.driver.WebDriverType;
 import org.openqa.selenium.WebDriver;
 
 public abstract class DriverFactory {
 
-    public WebDriver driver() {
-        return createDriver();
+    public static WebDriver getDriver(String driver) {
+        return switch (WebDriverType.valueOf(driver)) {
+            case CHROME -> new ChromeDriverFactory().createDriver();
+            case FIREFOX -> new FirefoxDriverFactory().createDriver();
+            case EDGE -> new EdgeDriverFactory().createDriver();
+            case SAFARI -> new SafariDriverFactory().createDriver();
+        };
     }
 
     protected abstract WebDriver createDriver();
