3fbd084024b02f06696b1ef7c67cbf294900b8bd
Piotr Gliszczynski <gliszczynskip5@gmail.com>
2023-12-17 14:19:12 +0100

    fix: add chromedriver file path and headless mode

--- src/main/java/com/qxam/auxiliary/driver/factory/ChromeDriverFactory.java
+++ src/main/java/com/qxam/auxiliary/driver/factory/ChromeDriverFactory.java
@@ -13,7 +13,12 @@ public class ChromeDriverFactory extends DriverFactory {
     ChromeOptions options = new ChromeOptions();
     options.addArguments("--start-maximized");
     options.addArguments("--dns-prefetch-disable");
+    options.addArguments("--headless");
+    options.addArguments("--no-sandbox");
+    options.addArguments("--disable-dev-shm-usage");
     options.addArguments("test-type");
+
+    System.setProperty("webdriver.chrome.driver", "./usr/local/bin/chromedriver.exe");
 
     WebDriverManager.getInstance(WebDriverType.CHROME.name()).setup();
     return new ChromeDriver(options);
