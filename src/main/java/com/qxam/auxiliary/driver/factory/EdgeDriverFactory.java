d5e5af01866e657669e6c00ea3666c226c022f8c
Piotr Gliszczynski <gliszczynskip5@gmail.com>
2023-12-20 10:03:41 +0100

    feat: add edge tests in github actions

--- src/main/java/com/qxam/auxiliary/driver/factory/EdgeDriverFactory.java
+++ src/main/java/com/qxam/auxiliary/driver/factory/EdgeDriverFactory.java
@@ -1,21 +1,24 @@
 package com.qxam.auxiliary.driver.factory;
 
 import com.qxam.auxiliary.driver.WebDriverType;
+import com.qxam.config.DataProvider;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import java.net.MalformedURLException;
+import java.net.URL;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class EdgeDriverFactory extends DriverFactory {
 
   @Override
-  protected WebDriver createDriver() {
+  protected WebDriver createDriver() throws MalformedURLException {
     EdgeOptions options = new EdgeOptions();
-    options.addArguments("--start-maximized");
     options.addArguments("--dns-prefetch-disable");
+    options.addArguments("--headless");
     options.addArguments("test-type");
 
     WebDriverManager.getInstance(WebDriverType.EDGE.name()).setup();
-    return new EdgeDriver(options);
+    return new RemoteWebDriver(new URL(DataProvider.get().webDriverUrl()), options);
   }
 }
